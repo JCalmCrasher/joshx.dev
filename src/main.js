d4d35ab5312d1c144db98cfd172c30691db888e9
JoshX [MacBookPro] <joshua.osagie01@gmail.com>
2021-10-16 00:54:51 +0100

    :sparkles: set background color

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import "@/assets/styles/main.css";
+import './assets/tailwind.css';
+import './App.css';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.mount('#app');
