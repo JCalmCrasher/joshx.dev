3bc18f356ee9addec26301d3adba709315e65a8e
JoshX [MacBookPro] <joshua.osagie01@gmail.com>
2022-04-18 05:17:59 +0100

    🚚 empty branch

--- src/schemas/index.js
+++
@@ -1,1 +0,0 @@
-export { default as GET_USER_POSTS } from "./posts";
--- src/utils/enum.js
+++
@@ -1,7 +0,0 @@
-const loadStatus = {
-  IDLE: "idle",
-  LOADING: "fetching",
-  FETCHED: "fetched",
-  ERROR: "error",
-};
-export default loadStatus;
